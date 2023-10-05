cc082e1d42734f4442cde744eaae1796ff68170d
Hailey Ahn <47484587+havocado@users.noreply.github.com>
2023-10-05 15:35:30 -0700

    Set up main for Raytracing (#5)
    Set up main.cpp for raytracing.
    
    - Rewrite main to only render once; the loop will only poll events such as window close
    - Rewrite vec3.h so vec3, point3, color uses float instead of double.
    - Add a stub color assignment function getColorPlaceholder
    - Fix minor typos/errors in vec3.h

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 #include "vec3.h"
 #include "color.h"
 
+// Placeholder for raytracing.
+color getColorPlaceholder(const float& NDC_x, const float& NDC_y) {
+    color c;
+    c[0] = (NDC_x+1.f)*0.5f;
+    c[1] = (NDC_y+1.f)*0.5f;
+    c[2] = 1.f;
+    return c;
+}
+
 int main() {
     GLFWwindow* window;
 
@@ -31,23 +40,31 @@ int main() {
     ratio = (float)width / (float)height;
     std::cout << "framebuffer sizes: width " << width << ", height " << height << std::endl;
 
-    // Loop
-    while (!glfwWindowShouldClose(window)) {
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+    // Set background color to viewport
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-        // Begin mode: draw points
-        glBegin(GL_POINTS);
-        for (int i = 0; i < 2*width; i++) {
-            for (int j = 0; j < 2*height; j++) {
-                glColor3f((float)j/(float)height*0.5f,(float)i/(float)width*0.5f,1.f);
-                glVertex2f((float)j/(float)height-1.f,(float)i/(float)width-1.f);
-            }
+    // Begin mode: draw points
+    glBegin(GL_POINTS);
+    for (int i = 0; i < 2*width; i++) {
+        for (int j = 0; j < 2*height; j++) {
+            float NDC_x = (float)j/(float)height-1.f; // [-1, 1]
+            float NDC_y = (float)i/(float)width-1.f; // [-1, 1]
+            color c = getColorPlaceholder(NDC_x, NDC_y);
+            //glColor3f((float)j/(float)height*0.5f,(float)i/(float)width*0.5f,1.f);
+            glColor3f(c.x(),c.y(),c.z());
+            glVertex2f(NDC_x,NDC_y);
         }
-        glEnd();
+    }
+    // End mode: draw points
+    glEnd();
 
-        // Swap front/back buffers
-        glfwSwapBuffers(window);
+    // Swap front/back buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    // Wait until the window is closed
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
--- vec3.h
+++ vec3.h
@@ -10,18 +10,18 @@
 
 class vec3 {
 public:
-    double e[3];
+    float e[3];
 
     vec3(): e{0,0,0} {}
-    vec3(double e0, double e1, double e2): e{e0, e1, e2} {}
+    vec3(float e0, float e1, float e2): e{e0, e1, e2} {}
 
-    double x() const { return e[0]; }
-    double y() const { return e[1]; }
-    double z() const { return e[2]; }
+    float x() const { return e[0]; }
+    float y() const { return e[1]; }
+    float z() const { return e[2]; }
 
-    vec3 operator-() const { return vec3(-e[0], -e[1], -e2[2]); }
-    double operator[](int i) const { return e[i]; }
-    double& operator[](int i) const { return e[i]; }
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    float operator[](int i) const { return e[i]; }
+    float& operator[](int i) { return e[i]; }
 
     vec3& operator+=(const vec3 &v) {
         e[0] += v.e[0];
@@ -30,22 +30,22 @@ public:
         return *this;
     }
 
-    vec3& operator*=(double t) {
+    vec3& operator*=(float t) {
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
         return *this;
     }
 
-    vec3& operator/=(double t) {
+    vec3& operator/=(float t) {
         return *this *= 1/t;
     }
 
-    double length() const {
+    float length() const {
         return std::sqrt(length_squared());
     }
 
-    double length_squared() const {
+    float length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 };
@@ -68,19 +68,19 @@ inline vec3 operator*(const vec3 &u, const vec3 &v) {
     return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
-inline vec3 operator*(double t, const vec3 &v) {
+inline vec3 operator*(float t, const vec3 &v) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
-inline vec3 operator*(const vec3 &v, double t) {
+inline vec3 operator*(const vec3 &v, float t) {
     return t * v;
 }
 
-inline vec3 operator/(const vec3 v, double t) {
+inline vec3 operator/(const vec3 v, float t) {
     return (1/t) * v;
 }
 
-inline double dot(const vec3 &u, const vec3 &v) {
+inline float dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
