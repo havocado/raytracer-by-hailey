06e9bac41bd11711367c14eb89c50e464cae8d53
Hailey <haa40@sfu.ca>
2023-10-20 22:12:15 -0700

    fixed files

--- color.h
+++ color.h
@@ -8,7 +8,7 @@
 #include "vec3.h"
 #include <iostream>
 
-using Color = vec3;
+using Color = Vec3;
 
 void writeColor(std::ostream &out, Color pixel_color) {
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
