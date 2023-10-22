f9364a9478bf4f21a0c8e11a6c68d96e95390cfd
Hailey <haa40@sfu.ca>
2023-10-21 23:12:58 -0700

    Materials implemented

---
+++ material.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 안혜영 on 2023-10-20.
+//
+
+#include "material.h"
---
+++ material.h
@@ -0,0 +1,14 @@
+//
+// Created by 안혜영 on 2023-10-20.
+//
+
+#ifndef RAYTRACER_BY_HAILEY_MATERIAL_H
+#define RAYTRACER_BY_HAILEY_MATERIAL_H
+
+
+class material {
+
+};
+
+
+#endif //RAYTRACER_BY_HAILEY_MATERIAL_H
